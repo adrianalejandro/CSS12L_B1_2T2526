85624a256d1e3497f1e74282ab39f925420154bb
serge03lopez-prog <serge03lopez@gmail.com>
2026-02-26 23:02:46 +0800

    Update mp4_serge_lopez.py

--- MP4 - String Operators/mp4_serge_lopez.py
+++ MP4 - String Operators/mp4_serge_lopez.py
@@ -1,1 +1,34 @@
+first_name = input("Enter your first name: ").strip()
+middle_name = input("Enter your middle name: ").strip()
+last_name = input("Enter your last name: ").strip()
 
+first_name = first_name.capitalize()
+middle_initial = middle_name[0].capitalize()
+last_name = last_name.capitalize()
+
+print(f"Formatted Name: {last_name}, {first_name} {middle_initial}.")
+
+word = input("Enter a word: ")
+number = int(input("Enter a number: "))
+
+print("Output:")
+for i in range(1, number + 1):
+    print(word * i)
+
+sentence = input("Enter a sentence: ")
+
+characters = len(sentence)
+
+words = sentence.split()
+word_count = len(words)
+
+vowels = "aeiouAEIOU"
+vowel_count = 0
+
+for letter in sentence:
+    if letter in vowels:
+        vowel_count += 1
+
+print("Characters:", characters)
+print("Words:", word_count)
+print("Vowels:", vowel_count)
